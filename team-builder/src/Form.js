b1333889df06eef2ad868812f8e7606b5bd014e2
Angela Plank <angelaplanknm@gmail.com>
2020-07-14 17:57:45 -0600

    basic form created

---
+++ team-builder/src/Form.js
@@ -0,0 +1,64 @@
+import React,{useState} from 'react';
+
+const Form=(props=>{
+    const defaultState={name:'',email:'',role:'', githubHandle: ''};
+
+    const [teamMember, setTeamMember]=useState(defaultState);
+
+    const handleChange=event=>{
+        setTeamMember({...teamMember,[event.target.name]: event.target.value});
+      }
+    
+      const handleSubmit=event=>{
+        event.preventDefault();
+        props.setAddedMember([...props.addedMember,teamMember]);
+        setTeamMember(defaultState);
+      }
+    return(
+    <form>
+        <label>
+            Name 
+            <input 
+                type='text' 
+                placeHolder='Enter Full Name' 
+                name='name' 
+                id='nameInput' 
+                value={teamMember.name} 
+                onChange={handleChange}>
+            </input>
+        </label><br></br>
+        <label>
+            Email 
+            <input 
+                type='text' 
+                placeHolder='Enter Email' 
+                name='email' 
+                id='emailInput' 
+                value={teamMember.email} 
+                onChange={handleChange}>
+            </input>
+        </label><br></br>
+        <label htmlFor='team role'>
+            Role </label>
+            <input type="text" 
+             placeHolder='Enter Team Role' 
+                id='team role' 
+                name='role' 
+                onChange={handleChange}>
+                    </input>
+       <br></br>
+       <label htmlFor='githubHandle'>
+            Github Handle </label>
+            <input type="text" 
+             placeHolder='Enter Your Github Handle' 
+                id='githubHandle' 
+                name='githubHandle' 
+                onChange={handleChange}>
+                    </input>
+       <br></br>
+        <button type='submit' >Add Team Member</button>
+    </form>
+    )
+})
+
+export default Form;
